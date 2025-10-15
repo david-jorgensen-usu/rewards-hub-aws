970337320dc4eb03657ac48791b1fe434083854d
Ubuntu <ubuntu@ip-172-31-35-230.us-east-2.compute.internal>
2025-10-15 05:42:24 +0000

    Found extraneous ] in file

--- RewardsHub/RewardsHub/settings.py
+++ RewardsHub/RewardsHub/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-]
+
 
 ROOT_URLCONF = "RewardsHub.urls"
 
