6e961005a088c79df5383f873a94c11318827ad7
David Jorgensen <a02365616@usu.edu>
2025-10-15 23:20:02 +0000

    Forgot to change views.py to update feedback

--- RewardsHub/core/urls.py
+++ RewardsHub/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("test-api/", views.test_api, name="test_api")
+    path("feedback/", views.feedback_api, name="feedback_api"),
 ]
