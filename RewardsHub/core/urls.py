b5a6082583c0e6f7e201d1e25fde1ea348adb2d0
David Jorgensen <a02365616@usu.edu>
2025-10-15 06:08:20 +0000

    Added a test-api url

--- RewardsHub/core/urls.py
+++ RewardsHub/core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("test-api/", views.test_api, name="test_api")
 ]
--- RewardsHub/core/views.py
+++ RewardsHub/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest, JsonResponse
 
 # Create your views here.
 def index(request: HttpRequest):
     return render(request, "core/index.html")
+
+def test_api(request: HttpRequest):
+    return JsonResponse({"message": "Hello from RewardsHub!"})
