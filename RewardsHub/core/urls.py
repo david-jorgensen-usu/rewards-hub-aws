6ec3e489f9814f6d55734dbba275456ed7326857
David Jorgensen <a02365616@usu.edu>
2025-10-15 23:30:55 +0000

    Testing fast commit

--- RewardsHub/core/urls.py
+++ RewardsHub/core/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("test-api/", views.test_api, name="test_api")
+    path("test-api/", views.test_api, name="test_api"),
     path("feedback/", views.feedback_api, name="feedback_api"),
 ]
