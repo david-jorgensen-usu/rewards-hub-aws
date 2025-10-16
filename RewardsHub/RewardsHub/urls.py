7c8eaf6225f04a3523881a4c1daa54f203d863e0
David Jorgensen <a02365616@usu.edu>
2025-10-16 20:50:21 +0000

    Updated main urls.py file

--- RewardsHub/RewardsHub/urls.py
+++ RewardsHub/RewardsHub/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("core.urls")),
     path("registration/", include("registration.urls")),
+    path("api/", include("api.urls")),
 ]
