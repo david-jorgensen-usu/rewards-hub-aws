c0ca8679cca11f6f83a49a5532ea70ea72d928ce
David Jorgensen <a02365616@usu.edu>
2025-10-17 00:39:17 +0000

    Corrected views mixup

--- RewardsHub/api/urls.py
+++ RewardsHub/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views, current_user
+from . import views
 
 urlpatterns = [
     path("test-api/", views.test_api, name="test_api"),
     path("feedback/", views.feedback_api, name="feedback_api"),
-    path("user/", current_user, name="current_user"),
+    path("user/", views.current_user, name="current_user"),
 ]
