3c2efc1406dac82355b451c5eef483701fa3c91e
David Jorgensen <a02365616@usu.edu>
2025-11-06 06:49:00 +0000

    Fixed api/

--- RewardsHub/api/urls.py
+++ RewardsHub/api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("feedback/", views.feedback_api, name="feedback_api"),
     path("user/", views.current_user, name="current_user"),
     path("user/programs/", views.programs, name="programs"),
-    path("api/link-app/", views.link_app, name="link_app"),
+    path("link-app/", views.link_app, name="link_app"),
 ]
